8fe9a2c083c0924c641a4d767608b0cb16d8d91f
abhijeet <abhijeet@eazotel.com>
2025-01-29 14:20:13 +0530

    up

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
           <>
             <Routes>
               <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="*" element={<Navigate to="/login" replace />} />
               <Route path="/login" element={<Login auth={auth} />} />
               <Route path="/home" element={<Home auth={auth} />} />
               <Route path="/not-found" element={<NotFound />} />
